4df3bbdaa06f467f30f32930bc2ce93098d4c88c
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-12-14 16:03:02 -0600

    added create, file, edit menu
    and added a status bar

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ root.geometry("1200x660")
 root.iconbitmap(r'textIcon.ico')
 
 #Create Main Frame
-
 my_frame = Frame(root)
 
 my_frame.pack(pady=5)
 
 #Create Scrollbar for Text Box
-
 text_scroll = Scrollbar(my_frame)
 text_scroll.pack(side=RIGHT, fill=Y)
 
@@ -31,6 +29,37 @@ my_text = Text(my_frame, width=97, height=25,
 text_scroll.config(command=my_text.yview)
 
 my_text.pack()
+
+#Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#Add File Menu
+file_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open")
+file_menu.add_command(label="Save")
+file_menu.add_separator()
+file_menu.add_command(label="Exit",command=root.quit)
+
+
+#Add Edit Menu
+edit_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy")
+edit_menu.add_command(label="Paste")
+edit_menu.add_command(label="Undo")
+edit_menu.add_command(label="Redo")
+
+
+
+
+#Add Status Bar at bottom
+status_bar = Label(root,text='Ready  ',anchor=E)
+status_bar.pack(fill=X,side=BOTTOM,ipady=5)
+
 
 
 
