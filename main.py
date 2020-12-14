7947412c088044f664ff0f1e08143cebe784e192
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-12-14 14:32:20 -0600

    added template and icon

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
+#tkinter window
+root = Tk()
+root.title("Santi's Text Editor")
+root.geometry("1200x")
+root.iconbitmap(r'textIcon.ico')
+
+
+
+
+root.mainloop()
